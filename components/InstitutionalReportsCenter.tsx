ac9fedb3dcb918a461d9b41b1e6ed9a95e265f2b
agenceaiacpff <84762273+agenceaiacpff@users.noreply.github.com>
2026-08-16 14:04:41 +0100

    Redeploy reports center V5 after build-rate window

--- components/InstitutionalReportsCenter.tsx
+++ components/InstitutionalReportsCenter.tsx
@@ -1,3 +1,4 @@
 "use client";
 
+// Centre des rapports V5 : sections repliables, exports autonomes et publication publique fidèle.
 export { default } from "@/components/InstitutionalReportsCenterV5";
