4a1c9aea4bfa21bb27ecc16648ba3206210970dd
agenceaiacpff <84762273+agenceaiacpff@users.noreply.github.com>
2026-08-16 15:12:42 +0100

    Activate audited professional messaging V2

--- components/MessageCenter.tsx
+++ components/MessageCenter.tsx
@@ -2,9 +2,9 @@
 
 import dynamic from "next/dynamic";
 
-const MessageCenter = dynamic(() => import("./MessageCenterHeavy"), {
-  loading: () => <div className="portalPanel"><p>Chargement de la messagerie…</p></div>,
+const MessageCenter = dynamic(() => import("./MessageCenterV2"), {
+  loading: () => <div className="portalPanel"><p>Chargement de la messagerie sécurisée…</p></div>,
 });
 
 export default MessageCenter;
-export type { ConversationRow, MessageRecipient } from "./MessageCenterHeavy";
+export type { ConversationRow, MessageRecipient } from "./MessageCenterV2";
