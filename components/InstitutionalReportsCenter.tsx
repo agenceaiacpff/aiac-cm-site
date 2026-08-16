1cf1ef12df4f4d487db8ce161711e37eea0e5cd9
agenceaiacpff <84762273+agenceaiacpff@users.noreply.github.com>
2026-08-16 14:08:29 +0100

    Redeploy reports center V5 after Vercel rate limit

--- components/InstitutionalReportsCenter.tsx
+++ components/InstitutionalReportsCenter.tsx
@@ -1,4 +1,4 @@
 "use client";
 
-// Centre des rapports V5 : sections repliables, exports autonomes et publication publique fidèle.
+// Centre des rapports V5 : sections repliables, exports autonomes, publication publique fidèle et historique des tâches terminé inclus.
 export { default } from "@/components/InstitutionalReportsCenterV5";
