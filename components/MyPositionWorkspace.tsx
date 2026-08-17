ffffdd967fcccaf7ea96322e2513ad5496bcbc8e
agenceaiacpff <84762273+agenceaiacpff@users.noreply.github.com>
2026-08-17 01:50:18 +0100

    Validate final My Position searchable body scope

--- components/MyPositionWorkspace.tsx
+++ components/MyPositionWorkspace.tsx
@@ -1,4 +1,4 @@
 "use client";
 
-// Mon poste V3 : audit final des droits, recherches, notifications, traçabilité et super-administration.
+// Mon poste V3 : audit final des droits, recherches, notifications, traçabilité, sélecteurs d’organe et super-administration.
 export { default } from "@/components/MyPositionWorkspaceV3";
